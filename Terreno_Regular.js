08d0044e7b294404361927ccf7139410c6a91d76
XimenaRubio212 <ximena.rubio@email.com>
2025-10-06 17:44:44 -0500

    actualizacion de codigo del primer ejercicio

--- Terreno_Regular.js
+++ Terreno_Regular.js
@@ -1,9 +1,10 @@
-let base = parseInt(prompt("Ingrese la base del rectángulo:"));
+// let base = parseInt(prompt("Ingrese la base del rectángulo:"));
 
-let altura = parseInt(prompt("Ingrese la altura del rectangulo"));
+// let altura = parseInt(prompt("Ingrese la altura del rectangulo"));
 
-let area = base*altura
+// let area = base*altura
 
-let perimetro = 2*(base+altura)
+// let perimetro = 2*(base+altura)
 
-alert("El área es= " + area + " y el perímetro es: " + perimetro)
+// alert("El área es= " + area + " y el perímetro es: " + perimetro)
+

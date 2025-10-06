f20a88109d70a6011b3bf35396274ab8f5c8944d
Alejandro estudie más <liamdreameater@gmail.com>
2025-10-06 17:13:47 -0500

    Creación del html y JavaScript de los 4 ejercicios

---
+++ Terreno_Regular.js
@@ -0,0 +1,9 @@
+let base = parseInt(prompt("Ingrese la base del rectángulo:"));
+
+let altura = parseInt(prompt("Ingrese la altura del rectangulo"));
+
+let area = base*altura
+
+let perimetro = 2*(base+altura)
+
+alert("El área es= " + area + " y el perímetro es: " + perimetro)
